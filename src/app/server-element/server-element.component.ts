5cf54a62d050749db2d6f4ca2e6758616bb6473c
Michal <michalgrzegorczyk04@gmail.com>
2022-02-21 19:14:46 +0100

    test lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +19,53 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called!');
+  }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log('%cngDoCheck called', 'color: green; font-size: 20px;');
+  }
+
+  ngAfterContentInit() {
+    console.log('%cngAfterContentInit called', 'color: red; font-size: 20px;')
+  }
+
+  ngAfterContentChecked() {
+    console.log('%cngAfterContentChecked called', 'color: brown; font-size: 20px;')
+  }
+
+  ngAfterViewInit() {
+    console.log('%cngAfterViewInit called', 'color: yellow; font-size: 20px;')
+  }
+
+  ngAfterViewChecked() {
+    console.log('%cngAfterViewChecked called', 'color: pink; font-size: 20px;')
+  }
+
+  ngOnDestroy() {
+    console.log('%cngOnDestroy called', 'color: purple; font-size: 20px;')
   }
 
 }
